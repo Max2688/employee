57a6c64b42ab6ffa561c6f4fde56dfd3095b9dcf
max2688 <jumper2388@gmail.com>
2020-08-05 18:51:08 +0300

    fix main page

--- routes/web.php
+++ routes/web.php
@@ -12,13 +12,13 @@
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('auth.login');
 });
 
 Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
-Route::group(['middleware' => ['web','auth']], function () {
+//Route::get('/home', 'HomeController@index')->name('home');
+Route::group(['prefix'=>'admin','middleware' => ['web','auth']], function () {
     Route::resource('company', "CompanyController");
     Route::resource('employee', "EmployeeController");
 });
